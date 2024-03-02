b61102c4950852e72641ec5e9aeb6bbc57d04400
antipersona <dla.cruz2002@gmail.com>
2024-03-02 18:52:50 +0100

    Rexpuesta

---
+++ src/main/java/es/ucm/fdi/iw/model/bd/Respuesta.java
@@ -0,0 +1,59 @@
+package es.ucm.fdi.iw.model.bd;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Respuesta {
+    @Id
+    private long id;
+    
+    @ManyToOne
+    private Valoracion valoracion;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    private String comentario;
+
+    public Respuesta(long id, Valoracion valoracion, Usuario usuario, String comentario) {
+        this.id = id;
+        this.valoracion = valoracion;
+        this.usuario = usuario;
+        this.comentario = comentario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Valoracion getValoracion() {
+        return valoracion;
+    }
+
+    public void setValoracion(Valoracion valoracion) {
+        this.valoracion = valoracion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+}
