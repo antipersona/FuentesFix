29ee640022f88953a5956c40aa1388852a839f87
antipersona <dla.cruz2002@gmail.com>
2024-03-05 14:04:00 +0100

    juandi pullea

--- src/main/java/es/ucm/fdi/iw/SecurityConfig.java
+++ src/main/java/es/ucm/fdi/iw/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.ignoringAntMatchers("/api/**")
 				.and()
 	        .authorizeRequests()
-	            .antMatchers("/css/**", "/js/**", "/img/**", "/", "/error").permitAll()
+	            .antMatchers("/css/**", "/js/**", "/img/**", "/", "/error", "/register", "/fuente/**").permitAll()
 				.antMatchers("/api/**").permitAll()            // <-- public api access
 				.antMatchers("/admin/**").hasRole("ADMIN")	   // <-- administration
 	            .antMatchers("/user/**").hasRole("USER")	   // <-- logged-in users
