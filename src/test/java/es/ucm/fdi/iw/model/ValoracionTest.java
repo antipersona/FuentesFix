2e09c57b753b95d5fdb947909dfc44958ff39eda
Ianal <ianalvarezdf@gmail.com>
2024-03-11 20:17:42 +0100

    pq no funciona estooooo aweawawa

--- src/test/java/es/ucm/fdi/iw/model/ValoracionTest.java
+++ src/test/java/es/ucm/fdi/iw/model/ValoracionTest.java
@@ -2,31 +2,52 @@ package es.ucm.fdi.iw.model;
 import es.ucm.fdi.iw.model.bd.Fuente;
 import es.ucm.fdi.iw.model.bd.Valoracion;
 
+import java.time.Duration;
+
 import javax.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import com.intuit.karate.Http;
-
-public class ValoracionTest {
+@SpringBootTest
+class ValoracionTest {
 
     @Test
-    public void testFuenteReceivesValoracion() {
-        // Create a new User
+    public void testFuenteReceivesValoracion() { // Test comprueba que la fuente recibe la valoración
         User usuario = new User();
-
-
-        // Create a new Fuente
         Fuente fuente = new Fuente();
-
-        // Create a new Valoracion
         Valoracion valoracion = new Valoracion(fuente, usuario, 5, 5, 5, 5, "Comentario");
-
-        // Set the Fuente for the Valoracion
         valoracion.setFuente(fuente);
-
-        // Assert that the Fuente in the Valoracion is the same as the created Fuente
         Assertions.assertEquals(fuente, valoracion.getFuente());
+    }
+
+    @Test
+    public void testAverageRatings() {
+        User usuario = new User();
+        Fuente fuente = new Fuente();
+        
+        Valoracion valoracion1 = new Valoracion(fuente, usuario, 5, 5, 5, 5, "Comentario 1");
+        Valoracion valoracion2 = new Valoracion(fuente, usuario, 4, 3, 2, 1, "Comentario 2");
+        Valoracion valoracion3 = new Valoracion(fuente, usuario, 2, 3, 4, 5, "Comentario 3");
+
+        valoracion1.setFuente(fuente);
+        valoracion2.setFuente(fuente);
+        valoracion3.setFuente(fuente);
+
+        fuente.añadirValoracion(valoracion1);
+        fuente.añadirValoracion(valoracion2);
+        fuente.añadirValoracion(valoracion3);
+        
+        double expectedGeneralRating = (5 + 4 + 2) / 3.0;
+        double expectedCaudalRating = (5 + 3 + 3) / 3.0;
+        double expectedSaborRating = (5 + 2 + 4) / 3.0;
+        double expectedTemperaturaRating = (5 + 1 + 5) / 3.0;
+        
+        Assertions.assertEquals(expectedGeneralRating, fuente.getMediaValoracionGeneral(), 0.01);
+        Assertions.assertEquals(expectedCaudalRating, fuente.getMediaValoracionCaudal(), 0.01);
+        Assertions.assertEquals(expectedSaborRating, fuente.getMediaValoracionSabor(), 0.01);
+        Assertions.assertEquals(expectedTemperaturaRating, fuente.getMediaValoracionTemperatura(), 0.01);
     }
 }
