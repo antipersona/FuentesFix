82bf58c2a54827f3bcfee9430693658a8c797c94
amonbofis <gabriel.zamy15@gmail.com>
2024-05-21 15:23:54 +0200

    improve of take in charge a report

--- src/main/resources/static/js/report.js
+++ src/main/resources/static/js/report.js
@@ -1,27 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const cogerEnCarga = document.getElementById('coger_en_carga');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const cogerEnCarga = document.getElementById('coger_en_carga');
 
     
-    cogerEnCarga.addEventListener('click', function() {
-        var reporteId = repId; // Get the Reporte ID from the data attribute
-        var funcionarioId = user_id; //should take it from fuente.html but doesn't work
-        var currentURL = window.location.pathname;
-        console.log("url = ",currentURL);
+// });
+    
+    // cogerEnCarga.addEventListener('click', function() {
         
-        // Send an AJAX request to update the Reporte object
-        var data = {
-            reporteId: reporteId,
-            funcionarioId: funcionarioId
-        };
+//});
 
-        go(currentURL, 'PUT', data)
-            .then(response => {
-                // Handle success response
-                console.log('Reporte updated successfully', response);
-            })
-            .catch(error => {
-                // Handle error
-                console.error('Failed to update Reporte', error);
-            });
-    });
-});
+function takerep(repId){
+    var reporteId = repId; // Get the Reporte ID from the data attribute
+    var funcionarioId = user_id; //should take it from fuente.html but doesn't work
+    var currentURL = window.location.pathname;
+    console.log("url = ",currentURL);
+    
+    // Send an AJAX request to update the Reporte object
+    var data = {
+        reporteId: reporteId,
+        funcionarioId: funcionarioId
+    };
+
+    go(currentURL, 'PUT', data)
+        .then(response => {
+            // Handle success response
+            const commentCell = document.getElementById(repId);
+            if (commentCell) {
+                commentCell.textContent = "Un funcionario ya esta occupendo se";
+            }
+            console.log('Reporte updated successfully', response);
+        })
+        .catch(error => {
+            // Handle error
+            console.error('Failed to update Reporte', error);
+        });
+}
